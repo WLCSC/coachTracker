4bd38fcfba69829bd38631e6cedbc81e95a89935
Sammidysam <sammidysam@gmail.com>
2015-06-11 13:35:42 -0400

    Sort lines

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,8 +1,8 @@
 class Position < ActiveRecord::Base
-	belongs_to :year
-	belongs_to :role
 	belongs_to :person
+	belongs_to :role
 	belongs_to :sport
+	belongs_to :year
 
 	def category
 		role.category if role
